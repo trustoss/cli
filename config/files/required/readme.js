b10bb868531b022f63a3664013c9b3419b1b027c
Trent Oswald <trentoswald@therebelrobot.com>
2016-08-16 15:29:49 -0700

    feat(config): working at refactoring file definitions

---
+++ config/files/required/readme.js
@@ -0,0 +1,32 @@
+const requireDir = require('require-dir')
+
+const README = {
+  id: 'readme',
+  filenames: [
+    'README',
+    'README.md',
+    'README.txt',
+    'ReadMe',
+    'ReadMe.md',
+    'ReadMe.txt',
+    'readme',
+    'readme.md',
+    'readme.txt'
+  ],
+  purpose: '"Landing page" of the repository, the first place new developers will look for information about the project, installation guide (or link to them), usage, troubleshooting, etc.',
+  versioncheck: 'minor',
+  templates: requireDir('../../templates/README'),
+  defaults: {
+    type: 'TrustOSS v0.1',
+    location: '../'
+  },
+  prompts: {
+    init: {
+      description: 'Readme Type',
+      type: 'string',
+      require: true
+    }
+  }
+}
+
+module.exports = README
