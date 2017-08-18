bd48ad16fb1b931d3224690d1f521aacf1463e2d
Trent Oswald <trentoswald@therebelrobot.com>
2017-08-18 08:48:36 -0700

    initial pass at rewrite

--- config/templates/index.js
+++
@@ -1,9 +0,0 @@
-var requireDir = require('require-dir')
-
-module.exports = {
-  readme: requireDir('./readme'),
-  changelog: requireDir('./changelog'),
-  coc: requireDir('./coc'),
-  contributing: requireDir('./contributing'),
-  license: requireDir('./license')
-}
--- helpers/log.js
+++
@@ -1,5 +0,0 @@
-module.exports = function log () {
-  if (process.env.VERBOSE) {
-    console.log.apply(console, arguments)
-  }
-}
